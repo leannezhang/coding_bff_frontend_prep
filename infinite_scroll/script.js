5026f3699d28925ca8fe4f732459489ac19d1c71
Leanne <lzhang800@gmail.com>
2024-05-02 12:06:03 -0700

    update code
    update code

--- infinite_scroll/script.js
+++ infinite_scroll/script.js
@@ -1,5 +1,16 @@
 const container = document.querySelector('.container');
 
+
+const createImagesDOM = (data) => {
+    const image = document.createElement('img');
+    image.src = data.message;
+    image.alt = "dog photo"
+    // Step 7: Optimize image loading.
+    image.loading = 'lazy'
+    container.appendChild(image);
+}
+
+// Step 5: adding loading states
 const loadingSpinner = () => {
     const loadingDiv = document.createElement("img");
     loadingDiv.className = "loading";
@@ -7,25 +18,15 @@ const loadingSpinner = () => {
     container.append(loadingDiv);
 }
 
-const createImageEle = (data) => {
-    const image = document.createElement('img');
-    image.src = data.message;
-    image.alt = "photo"
-    // Step 6: Optimize image loading..
-    image.loading = 'lazy'
-    container.appendChild(image);
-}
-
 // Step 2: Load Images 
 async function loadImages(numImages = 9) {
     for (let i=0; i < numImages; i++) {
         try {
-            // Step 3: adding loading states
             loadingSpinner();
             const response = await fetch('https://dog.ceo/api/breeds/image/random')
             const data = await response.json();
             document.querySelector('.loading').remove();
-            createImageEle(data);
+            createImagesDOM(data);
         } catch (error) {
             console.log(error);
         }
@@ -33,13 +34,12 @@ async function loadImages(numImages = 9) {
 }
 
 
-
+// Step 3: Add handle scroll
 const handleScroll = () => {
     console.log('innerHeight', window.innerHeight);
     console.log('scrollY', window.scrollY);
     console.log('container.scrollHeight', container.scrollHeight);
     
-    const PRELOAD_HEIGHT = 200;
     if (window.innerHeight + window.scrollY  >= container.scrollHeight) {
         console.log('Loading more images');
         loadImages();
@@ -47,45 +47,30 @@ const handleScroll = () => {
 }
 
 // Step 6: add throttle
-// within 2s only execute once
+// only execute the callback once WITHIN 2s
 // limit how often can be executed in response to an event such as window resizing or scrolling
-const throttle = (func, limit) => {
+const throttle = (callback, limit) => {
     let lastFunc;
     let lastRan;
     return function() {
-        const context = this;
         const args = arguments;
         if (!lastRan) {
-            func.apply(context, args);
+            callback(...args);
             lastRan = Date.now();
         } else {
             clearTimeout(lastFunc);
             lastFunc = setTimeout(function() {
                 if ((Date.now() - lastRan) >= limit) {
-                    func.apply(context, args);
+                    callback(...args);
                     lastRan = Date.now();
                 }
             }, limit - (Date.now() - lastRan));
         }
     };
 }
-// Step 6: wait till 2s than exectuate
-const debounce = (callback, delay) => {
-    let timer;
-    return function(...arguments) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            callback(...arguments);
-        }, delay);
-    }
-}
+
 
 loadImages();
-// Step 4: add event listening to listen on scroll
-window.addEventListener('scroll', throttle(handleScroll, 2000));
-
-
-
-
-
-
+// Step 3: add event listening to listen on scroll
+const DELAY = 2000;
+window.addEventListener('scroll', throttle(handleScroll, DELAY));
